7350fb91a86526ccff88f9d528b831f7d6ed286b
Nguyen-David <devid.nguyen21@gmail.com>
2018-03-27 01:08:08 +0300

    Fifth commit

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-	$(".nav-link").on("click",function(){
-		$('.slick').slick('setPosition');
-	});
+    $(".button-up").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href');
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        var top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').stop().animate({'scrollTop': top}, 1500);
+    });
 });
